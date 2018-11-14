4da0d75c18ba1c056cc5ba3fb88244c6ececa91c
tung.nguyen@2clicksolutions.com <tung.nguyen@2clicksolutions.com>
2018-11-14 12:34:08 +0700

    update version

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Subscription } from 'rxjs/Subscription';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-template-form',
